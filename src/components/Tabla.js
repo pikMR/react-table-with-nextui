d7efbf1e7f38ca91f8a57e879bb86a8a2f2e1a1e
pikmr <benjahergar@gmail.com>
2024-02-24 00:57:09 +0100

    add icons to add and memo to filter and state to add

--- src/components/Tabla.js
+++ src/components/Tabla.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
+import { AddIcon } from "./icons/AddIcon";
 import { DeleteIcon } from "./icons/DeleteIcon";
+import { CheckIcon } from "./icons/CheckIcon";
+import { SearchIcon } from "./icons/SearchIcon";
 import { EditIcon } from "./icons/EditIcon";
 import { Tooltip } from "@nextui-org/tooltip";
+import { Button } from "@nextui-org/button";
 import { Input } from "@nextui-org/input";
 import { parseDate } from "@internationalized/date";
 import {
@@ -14,7 +18,40 @@ import {
 } from "@nextui-org/table";
 
 const Tabla = ({ children }) => {
-const [editModes, setEditModes] = useState({});
+  const [filterValue, setFilterValue] = React.useState("");
+  const hasSearchFilter = Boolean(filterValue);
+  const [editModes, setEditModes] = useState({});
+
+      const onClear = React.useCallback(() => {
+        setFilterValue("");
+      }, []);
+
+      const onSearchChange = React.useCallback((value) => {
+        if (value) {
+          setFilterValue(value);
+        } else {
+          setFilterValue("");
+        }
+      }, []);
+  
+  const topContent = React.useMemo(() => {
+    return (
+      <div className="flex justify-between gap-3 items-center">
+        <Button color="primary" endContent={<AddIcon />}>
+          Nuevo Extracto
+        </Button>
+        <Input
+          isClearable
+          className="w-11/12"
+          placeholder="Buscar Extracto..."
+          startContent={<SearchIcon />}
+          value={filterValue}
+          onClear={() => onClear()}
+          onValueChange={onSearchChange}
+        />
+      </div>
+    );
+  }, [filterValue, onSearchChange, onClear]);
 
   const handleEditClick = (rowKey) => {
     // Actualiza el estado del modo de edición específico de la fila al hacer clic en Editar
@@ -23,9 +60,27 @@ const [editModes, setEditModes] = useState({});
       [rowKey]: !prevEditModes[rowKey], // Toggle el modo de edición para la fila
     }));
   };
- const datos = Array.isArray(children) ? children : [];
+
+  const datos = Array.isArray(children) ? children : [];
+  
+    const filteredItems = React.useMemo(() => {
+      let filteredExtracts = [...datos];
+
+      if (hasSearchFilter) {
+        filteredExtracts = filteredExtracts.filter((user) =>
+          user.name.toLowerCase().includes(filterValue.toLowerCase())
+        );
+      }
+
+      return filteredExtracts;
+    }, [datos, filterValue, hasSearchFilter]);
+  
   return (
-    <Table aria-label="Example static collection table">
+    <Table
+      aria-label="Example static collection table"
+      topContent={topContent}
+      topContentPlacement="outside"
+    >
       <TableHeader>
         <TableColumn>Fecha</TableColumn>
         <TableColumn>Descripcion</TableColumn>
@@ -100,6 +155,11 @@ const [editModes, setEditModes] = useState({});
                 <Tooltip color="danger" content="Eliminar Extracto">
                   <span className="text-lg text-danger cursor-pointer active:opacity-50">
                     <DeleteIcon />
+                  </span>
+                </Tooltip>
+                <Tooltip color="success" content="Actualizar Extracto">
+                  <span className="whitespace-pre text-lg text-success cursor-pointer active:opacity-50">
+                    <CheckIcon />
                   </span>
                 </Tooltip>
               </div>

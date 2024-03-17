b881a45a5b179a85dd093fd055082016ce9cc9c6
pikmr <benjahergar@gmail.com>
2024-03-17 21:48:32 +0100

    resize up icons

--- src/components/icons/CheckIcon.jsx
+++ src/components/icons/CheckIcon.jsx
@@ -4,8 +4,8 @@ export const CheckIcon = (props) => (
     <svg
       fill="currentColor"
       viewBox="0 0 16 16"
-      height="1em"
-      width="3em"
+      height="2em"
+      width="2em"
       {...props}
     >
       <path d="M10.97 4.97a.75.75 0 011.07 1.05l-3.99 4.99a.75.75 0 01-1.08.02L4.324 8.384a.75.75 0 111.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 01.02-.022z" />
